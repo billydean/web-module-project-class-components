3e74c5cda5f283b9f75d63206395338d4ef6b01f
billydean <billy.goehring@gmail.com>
2022-03-07 10:55:38 -0800

    initial commit

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,25 @@
 import React from 'react'
+import { v4 as uuid } from 'uuid'
 
+const todos = [
+  {
+    name: '',
+    id: uuid(),
+    completed: false,
+  },
+  {
+    name: '',
+    id: uuid(),
+    completed: false,
+  }
+]
+const initialState = {
+  errorMessage: '',
+  todos,
+}
 export default class App extends React.Component {
+  state = initialState;
+  
   render() {
     return (
       <div>
